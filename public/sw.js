6c9fcce1c6707faad7b1211001f07147da24c81e
Pvrolomx <pvrolo@gmail.com>
2026-01-26 06:01:45 -0600

    Add service worker for PWA

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'condo-advisor-v1';
+const urlsToCache = [
+  '/',
+  '/logo_banner.png',
+  '/bg-condo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
